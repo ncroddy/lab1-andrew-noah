c906730d3cf8224b0133c873b17f54e4c9da00b7
Andrew Fischer <afischer5@luc.edu>
2017-09-11 22:41:02 +0000

    lab1 - part3

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -8,19 +8,26 @@ public class Main {
   
    if(args[0]!=null){
     input = Integer.parseInt(args[0]);
-    
-    if ( input <= 0) System.out.println("Need a positive input");
-    else{
-     for(int i=0; i<input; i++){
-      if(i%3 == 0 ) System.out.print("Fizz ");
-      if(i%5 == 0 )  System.out.print("Buzz");
-      if(i%3 != 0 && i%5 != 0) System.out.print(i);
-
-      System.out.println();
-      }
-    }
-    
-   }
    
+    for(int i = 0; i < input; i++){
+     System.out.println(fizzBuzzArray(input)[i]);  
+  
+    }
+   }
+  }
+  
+public static String[] fizzBuzzArray(final int n){
+ String[] output = new String [n];
+ 
+  if ( n <= 0) System.out.println("Need a positive input");
+    else{
+     for(int i=0; i<n; i++){
+      if(i%3 == 0 ) output[i]= "Fizz";
+      if(i%5 == 0 )  output[i] = "Buzz";
+      if(i%3 == 0 && i%5 == 0) output[i] = "Fizz Buzz";
+      if(i%3 != 0 && i%5 != 0) output[i] = Integer.toString(i);
+     }
+    }
+    return output;
   }
 }
