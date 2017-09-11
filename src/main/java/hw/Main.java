73e1e2ea5d1b5d96eb1969fcfaebd549f50808b8
Andrew Fischer <afischer5@luc.edu>
2017-09-11 14:24:41 +0000

    lab1 - part2

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -1,26 +1,26 @@
 package hw;
 
-import java.util.Scanner;
-
 public class Main {
 
   public static void main(String[] args) {
   
   int input;
-  Scanner keyboard = new Scanner(System.in);
+  
+   if(args[0]!=null){
+    input = Integer.parseInt(args[0]);
     
-  input = keyboard.nextInt(); 
-   
-   for(int i=0; i<input; i++){
+    if ( input <= 0) System.out.println("Need a positive input");
+    else{
+     for(int i=0; i<input; i++){
       if(i%3 == 0 ) System.out.print("Fizz ");
       if(i%5 == 0 )  System.out.print("Buzz");
       if(i%3 != 0 && i%5 != 0) System.out.print(i);
 
       System.out.println();
       }
-      
-    keyboard.close(); 
+    }
     
-    return;
+   }
+   
   }
 }
